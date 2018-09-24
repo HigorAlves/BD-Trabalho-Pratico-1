ea9eb2f76d942176c790b78467b3e22d0449ab54
Higor Alves <Goodyearalves@gmail.com>
2018-09-24 14:55:30 -0300

    Init com NavBar

---
+++ website/src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+	render() {
+		return (
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+				<a className="navbar-brand" href="">
+					Trabalho Pratico
+				</a>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-toggle="collapse"
+					data-target="#navbarTogglerDemo01"
+					aria-controls="navbarTogglerDemo01"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon" />
+				</button>
+				<div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+					<ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+						<li className="nav-item active">
+							<a className="nav-link" href="">
+								Home <span className="sr-only">(current)</span>
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Login
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link" href="#">
+								Cadastrar
+							</a>
+						</li>
+					</ul>
+					<form className="form-inline my-2 my-lg-0">
+						<input
+							className="form-control mr-sm-2"
+							type="search"
+							placeholder="Procurar"
+							aria-label="Procurar"
+						/>
+						<button
+							className="btn btn-outline-success my-2 my-sm-0"
+							type="submit"
+						>
+							Procurar
+						</button>
+					</form>
+				</div>
+			</nav>
+		);
+	}
+}
---
+++ website/src/index.js
@@ -0,0 +1,14 @@
+//Bootstrap Imports
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
