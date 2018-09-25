a7e51caca12ba977fe82225aa6a993434c91ef5d
Higor Alves <Goodyearalves@gmail.com>
2018-09-24 21:55:41 -0300

    Mudança no NavBar
    Navbar agora abre uma uma sideBar no local de um hamburguer

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -1,58 +1,40 @@
 import React, { Component } from 'react';
 import '../assets/css/navbar.css';
+import { SEARCH } from '../lib/fontAwesome';
 
 export default class Navbar extends Component {
 	render() {
 		return (
-			<nav className="navbar navbar-expand-lg navbar-dark navbar-primary">
-				<a className="navbar-brand" href="">
-					Trabalho Pratico
+			<div>
+				<nav className="navbar navbar-expand-lg navbar-dark navbar-primary">
+					<a className="navbar-brand" href="">
+						Trabalho Pratico
 				</a>
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-toggle="collapse"
-					data-target="#navbarTogglerDemo01"
-					aria-controls="navbarTogglerDemo01"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon" />
-				</button>
-				<div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-					<ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-						<li className="nav-item active">
-							<a className="nav-link" href="">
-								Home <span className="sr-only">(current)</span>
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								Login
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="#">
-								Cadastrar
-							</a>
-						</li>
-					</ul>
-					<form className="form-inline my-2 my-lg-0">
-						<input
-							className="form-control mr-sm-2"
-							type="search"
-							placeholder="Procurar"
-							aria-label="Procurar"
-						/>
-						<button
-							className="btn btn-outline-success my-2 my-sm-0"
-							type="submit"
-						>
-							Procurar
-						</button>
-					</form>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-toggle="collapse"
+						data-target="#navbarTogglerDemo01"
+						aria-controls="navbarTogglerDemo01"
+						aria-expanded="false"
+						aria-label="Toggle navigation"
+					>
+						<span className="navbar-toggler-icon" />
+					</button>
+				</nav>
+
+				<div className="collapsing navbar-collapse navbar-nav mr-auto mt-2 mt-lg-0" id="navbarTogglerDemo01">
+					<h5>10.658</h5>
+					<span>Recursos disponiveis</span>
+					<br />
+					<div className="input-group p-3">
+						<input type="text" className="form-control" placeholder="Pesquisar artigos disponiveis" aria-label="Recipient's username" aria-describedby="button-addon2" />
+						<div className="input-group-append">
+							<button className="btn btn-outline-secondary" type="button" id="button-addon2"><SEARCH /> Pesquisar</button>
+						</div>
+					</div>
 				</div>
-			</nav>
+			</div>
 		);
 	}
 }
--- website/src/lib/fontAwesome.js
+++ website/src/lib/fontAwesome.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
+import { faNewspaper, faSearch } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faNewspaper);
+library.add(faNewspaper, faSearch);
 
 export const NEWSPAPER = () => <FontAwesomeIcon icon="newspaper" size={'3x'} />;
+export const SEARCH = () => <FontAwesomeIcon icon="search" size={'1x'} />;
