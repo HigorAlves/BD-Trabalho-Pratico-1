e950b51429b8984e039f740d4b6e59a8cb16d44c
Higor Alves <Goodyearalves@gmail.com>
2018-09-27 14:39:45 -0300

    Navbar com table
    Inserida a table com as modificações necessarias pra mesma funcionar dentro do collapse bar.

--- website/src/components/Navbar.js
+++ website/src/components/Navbar.js
@@ -9,7 +9,7 @@ export default class Navbar extends Component {
 				<nav className="navbar navbar-expand-lg navbar-dark navbar-primary">
 					<a className="navbar-brand" href="">
 						Trabalho Pratico
-				</a>
+					</a>
 					<button
 						className="navbar-toggler"
 						type="button"
@@ -23,29 +23,77 @@ export default class Navbar extends Component {
 					</button>
 				</nav>
 
-				<section className="collapsing navbar-collapse navbar-nav mr-auto mt-2 mt-lg-0" id="navbarToggler">
-					<h5>10.658</h5>
+				<section
+					className="collapsing navbar-collapse navbar-nav mr-auto mt-2 mt-lg-0 d-lg-none d-xl-none"
+					id="navbarToggler"
+				>
+					<h5 className="semMargin">10.658</h5>
 					<span>Recursos disponiveis</span>
-					<div className="input-group p-3">
-						<input type="text" className="form-control" placeholder="Pesquisar artigos disponiveis" aria-label="Recipient's username" aria-describedby="button-addon2" />
+					<div className="input-group p-4">
+						<input
+							type="text"
+							className="form-control"
+							placeholder="Pesquisar artigos"
+							aria-label="Recipient's username"
+							aria-describedby="button-addon2"
+						/>
 						<div className="input-group-append">
-							<button className="btn btn-outline-secondary" type="button" id="button-addon2"><SEARCH /> Pesquisar</button>
+							<button
+								className="btn btn-outline-secondary"
+								type="button"
+								id="button-addon2"
+							>
+								<SEARCH /> Pesquisar
+							</button>
 						</div>
 					</div>
 
 					<section>
 						<div className="container-fluid">
 							<div className="row">
-								<div className="mx-auto p-3">
-									<div className="tableHeader">
-										<p>Visualizar por categoria</p>
-									</div>
-								</div>
+								<table className="table table-bordered table-hover table-sm">
+									<thead>
+										<tr>
+											<th>Visualizar por categorias</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>
+												<span>Exemplo</span>
+											</td>
+										</tr>
+									</tbody>
+								</table>
 							</div>
-
 						</div>
 					</section>
 
+					<section>
+						<div className="container-fluid">
+							<div className="row">
+								<table className="table table-bordered table-hover table-sm">
+									<thead>
+										<tr>
+											<th colSpan="2" align="justify">
+												Minha Conta
+											</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>
+												<span>Logar</span>
+											</td>
+											<td>
+												<span>Criar Conta</span>
+											</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</section>
 				</section>
 			</div>
 		);
