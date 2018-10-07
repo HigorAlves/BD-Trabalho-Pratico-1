bfa280e47170900609ef67d1600255882d1512b4
Higor Alves <Goodyearalves@gmail.com>
2018-10-07 20:17:15 -0300

    Novos Inserrs

--- API/index.js
+++ API/index.js
@@ -5,9 +5,13 @@ const mysql = require('mysql');
 const app = express();
 app.use(cors());
 
-
+//Querys usadas para realizar as consultas
 const SELECT_ALL_USUARIOS = 'SELECT * FROM heroku_7c4a5149dcb9e6c.tipo_usuario'
 const INSERT_USUARIO = 'INSERT INTO tipo_Usuario (cpf_usr, nome, senha, logradouro, numero, bairro, cep, datacad) VALUES (?,?,?,?,?,?,?,?)';
+const INSERT_ADMINISTRADOR = 'INSERT INTO administrador (cpf_adm, tipo_adm) VALUES (?,?)';
+const SELECT_ALL_ADMINISTRADORES = 'SELECT * FROM heroku_7c4a5149dcb9e6c.administrador';
+const INSERT_GRANDES_AREAS = 'INSERT INTO grandes_areas (IdGA, NomeGA,cpf_adm) VALUES (?,?,?)';
+const INSERT_AGENCIA_FOMENTO = 'INSERT INTO agenciafomento (IdAgenciaFomento, NomeAgenciaFomento, DataCad) VALUES'
 
 const connection = mysql.createConnection({
   host: 'us-cdbr-iron-east-01.cleardb.net',
@@ -26,6 +30,7 @@ app.get('/', (req, res) => {
   res.send('Ola mundo');
 });
 
+//Lista todos os Usuarios
 app.get('/usuarios', (req, res) => {
   connection.query(SELECT_ALL_USUARIOS, (error, results) => {
     if (error) {
@@ -37,8 +42,7 @@ app.get('/usuarios', (req, res) => {
     }
   })
 });
-
-///cadastrar?cpf_usr=034285601&nome=Higor&senha=123456&logradouro=rua&numero=12&bairro=centro&cep=35550000&datacad=07-11-2018
+//Cadastra um novo Usuario
 app.get('/usuarios/cadastrar', (req, res) => {
   const usuario = { cpf_usr, nome, senha, logradouro, numero, bairro, cep, datacad } = req.query;
 
@@ -49,8 +53,30 @@ app.get('/usuarios/cadastrar', (req, res) => {
       return res.send('Usuario cadastrado com sucesso');
     }
   })
+});
+
+//Lista todos os Administradores
+app.get('/administrador', (req, res) => {
+  connection.query(SELECT_ALL_ADMINISTRADORES, (error, results) => {
+    if (error) {
+      return res.send(error);
+    } else {
+      return res.send(results);
+    }
+  })
+})
+//Cadastra novo Administrador
+app.get('/administrador/cadastrar', (req, res) => {
+  const administrador = { cpf_adm, tipo_adm } = req.query;
+  connection.query(INSERT_ADMINISTRADOR, [administrador.cpf_adm, administrador.tipo_adm], (error, results) => {
+    if (error) {
+      return res.send(error);
+    } else {
+      return res.send('Administrador cadastrado com sucesso');
+    }
+  })
 })
 
-app.listen(4000, () => {
+app.listen(3000, () => {
   console.log('Executando na porta 4000');
 })
