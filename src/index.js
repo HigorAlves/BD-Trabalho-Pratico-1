dbe977f00b6be8786f6d2ad2d6f3aa06baa9f2b4
Higor Alves <Goodyearalves@gmail.com>
2018-10-07 11:14:52 -0300

    Adicionado Routers

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <Route path='/' component={App} />
+  </Router>
+
+  , document.getElementById('root'));
 registerServiceWorker();
